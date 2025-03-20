20dbdbafc96fdaf142800cad3e30d701662e9a54
TabSpace <pillar0514@163.com>
2025-03-20 15:48:46 +0800

    fix: 🐛 更正使用组件的名称

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const $build = require('./kit/build');
 const $print = require('./kit/print');
 const $resolve = require('./server/util/resolve');
 
-// poodle factory
-function factory(poodle) {
+// spore factory
+function factory(spore) {
   const port = Number(process.env.PORT) || 8090;
 
-  poodle.register('serve', async () => {
-    poodle.nodemon({
+  spore.register('serve', async () => {
+    spore.nodemon({
       script: $resolve('./kit/serve.js'),
       ext: 'js json',
     });
@@ -16,14 +16,14 @@ function factory(poodle) {
     description: 'development serve',
   });
 
-  poodle.register('build', async () => {
+  spore.register('build', async () => {
     await $build();
   }, {
     port,
     description: 'build app',
   });
 
-  poodle.register('print', async () => {
+  spore.register('print', async () => {
     await $print();
   }, {
     port,
